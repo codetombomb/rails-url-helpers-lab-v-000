8d7612428f79a490c82897c8f562315aaa37dbd0
Tom Tobar <tomas.tobar@flatironschool.com>
2021-06-16 22:00:44 +0000

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,5 +1,5 @@
 class StudentsController < ApplicationController
-  before_action :set_student, only: :show
+  before_action :set_student, only: [:show, :activate]
   
   def index
     @students = Student.all
@@ -7,6 +7,10 @@ class StudentsController < ApplicationController
 
   def show
     
+  end
+  
+  def activate
+   byebug
   end
 
   private
